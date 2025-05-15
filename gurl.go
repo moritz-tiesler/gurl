2b23df2512e481b4cae0069e67136f6bdde29b9d
Moritz Tiesler <moritz.tiesler@t-online.de>
2025-05-15 19:32:09 +0200

    redirect

--- gurl.go
+++ gurl.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	_ "embed"
 	"log"
+	"net/http"
 	"reflect"
+	"time"
 
 	"gurl/repository"
 	"gurl/repository/tutorial"
@@ -54,9 +56,64 @@ func run() error {
 	return nil
 }
 
-func main() {
-	if err := run(); err != nil {
-		log.Fatal(err)
+func NewServer() *http.Server {
+	router := http.NewServeMux()
+	router.HandleFunc("POST /url", postURL)
+	router.HandleFunc("GET /url", getURL)
 
+	stack := Stack(
+		LogRequestMiddleware(log.Printf),
+	)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: stack(router),
 	}
+
+	return server
+}
+
+type middleware func(http.Handler) http.Handler
+
+func Stack(middlewares ...middleware) middleware {
+	return func(next http.Handler) http.Handler {
+		for i := len(middlewares) - 1; i > -1; i-- {
+			m := middlewares[i]
+			next = m(next)
+		}
+		return next
+	}
+}
+
+func LogRequestMiddleware(loggingFunc func(string, ...any)) middleware {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			loggingFunc("%v: LOG %s - %s %s %s\n", time.Now(), r.RemoteAddr, r.Proto, r.Method, r.URL)
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func postURL(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("gurl.com/url/short"))
+}
+
+func getURL(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "https://www.zeit.de", http.StatusMovedPermanently)
+}
+
+func main() {
+	// if err := run(); err != nil {
+	// 	log.Fatal(err)
+
+	// }
+
+	s := NewServer()
+
+	log.Printf("launching server at %v", s.Addr)
+	if err := s.ListenAndServe(); err != nil {
+		log.Fatalf("Could not launch server")
+	}
+
 }
