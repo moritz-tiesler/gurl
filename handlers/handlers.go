ac28fcd2ee9765637d3e16bfffe6711dc63edfda
Moritz Tiesler <moritz.tiesler@t-online.de>
2025-05-16 16:54:13 +0200

    https prefix

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -82,7 +82,7 @@ func (h *Handler) PostURL(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	w.Write(fmt.Appendf(nil, "%s/url/%s", r.Host, shortURLKey))
+	w.Write(fmt.Appendf(nil, "https://www.%s/url/%s", r.Host, shortURLKey))
 }
 
 func (h *Handler) GetURL(w http.ResponseWriter, r *http.Request) {
