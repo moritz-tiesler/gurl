4b823786ba9ea7bb0b2fc2ba6991ba5be6093c98
Moritz Tiesler <moritz.tiesler@t-online.de>
2025-05-19 15:04:52 +0200

    journal_mode=WAL

--- repository/author_repo.go
+++ repository/author_repo.go
@@ -5,16 +5,23 @@ import (
 	"database/sql"
 	_ "embed"
 	"gurl/repository/tutorial"
+	"log"
 )
 
 func New(ctx context.Context, connString string, schema string) (*tutorial.Queries, error) {
 
 	db, err := sql.Open("sqlite", connString)
-	db.SetMaxOpenConns(1)
 	if err != nil {
 		return nil, err
 	}
-	// create tables
+
+	_, err = db.Exec("PRAGMA journal_mode=WAL;")
+	if err != nil {
+		log.Fatalf("Failed to set journal_mode: %v", err)
+	}
+
+	db.SetMaxOpenConns(1)
+
 	if _, err := db.ExecContext(ctx, schema); err != nil {
 		return nil, err
 	}
